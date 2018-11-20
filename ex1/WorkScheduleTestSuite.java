8df1356dd453e173417d525a952577daa0a09817
Johan CK Martinson <johan.t.martinson@gmail.com>
2018-11-20 13:07:15 +0100

    test suite done

--- ex1/WorkScheduleTestSuite.java
+++ ex1/WorkScheduleTestSuite.java
@@ -1,2 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({addWorkingPeriodTests.class, workingEmployeesTests.class})
 public class WorkScheduleTestSuite {
 }
